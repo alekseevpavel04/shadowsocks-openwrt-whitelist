440f850e36c5df311cadd2aeabf83d4928afeee6
alekseevpavel04 <alekceevpavel@mail.ru>
2026-02-16 22:44:57 +0300

    add filt

--- vpn-stop.bat
+++ vpn-stop.bat
@@ -1,15 +1,11 @@
 @echo off
-
 echo ========================================
 echo   VPN - STOP
 echo ========================================
 echo.
-
 set "ROUTER=192.168.8.1"
-
 echo Stopping VPN...
-ssh root@%ROUTER% "killall ss-redir 2>/dev/null; iptables -t nat -D PREROUTING -p tcp -j SS_REDIR 2>/dev/null; iptables -t nat -F SS_REDIR 2>/dev/null; iptables -t nat -X SS_REDIR 2>/dev/null; ipset flush vpn_list 2>/dev/null; rm -f /tmp/dnsmasq.d/vpn-whitelist.conf; /etc/init.d/dnsmasq restart >/dev/null 2>&1; echo 'VPN stopped.'"
-
+ssh root@%ROUTER% "killall ss-redir 2>/dev/null; iptables -t nat -D PREROUTING -p tcp -j SS_REDIR 2>/dev/null; iptables -t nat -F SS_REDIR 2>/dev/null; iptables -t nat -X SS_REDIR 2>/dev/null; iptables -t nat -D PREROUTING -p udp --dport 53 ! -d 192.168.8.1 -j DNAT --to-destination 192.168.8.1:53 2>/dev/null; iptables -t nat -D PREROUTING -p tcp --dport 53 ! -d 192.168.8.1 -j DNAT --to-destination 192.168.8.1:53 2>/dev/null; iptables -D FORWARD -p udp -m set --match-set vpn_list dst -j DROP 2>/dev/null; ipset flush vpn_list 2>/dev/null; rm -f /tmp/dnsmasq.d/vpn-whitelist.conf; /etc/init.d/dnsmasq restart >/dev/null 2>&1; echo 'VPN stopped.'"
 echo.
 echo ========================================
 echo   VPN is OFF
